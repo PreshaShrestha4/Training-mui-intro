5949b0907aa7943015dd8279e3e276178339a9d5
PreshaShrestha4 <preshashrestha041@gmail.com>
2025-01-30 12:10:32 +0545

    first commit

---
+++ mui-intro/src/App.jsx
@@ -0,0 +1,23 @@
+import { Box, Button, Typography } from "@mui/material";
+import React, { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <Box>
+      <Typography variant="h3">{count}</Typography>
+      <Button
+        variant="contained"
+        color="success"
+        onClick={() => {
+          const newCount = count + 1;
+          setCount(newCount);
+        }}
+      >
+        increase count
+      </Button>
+    </Box>
+  );
+};
+
+export default App;
