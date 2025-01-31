6d36141d5b62191cb1b7a7c3e102e43ade649645
PreshaShrestha4 <preshashrestha041@gmail.com>
2025-01-31 17:00:06 +0545

    update

--- mui-intro/src/App1.jsx
+++ mui-intro/src/App1.jsx
@@ -68,10 +68,10 @@ const App1 = () => {
       </Box>
       <Box>
         <Typography variant="h1">Heading 1</Typography>
-        <Typography variant="h2">Heading 2</Typography>
+        {/* <Typography variant="h2">Heading 2</Typography>
         <Typography variant="h3">Heading 3</Typography>
         <Typography variant="h4">Heading 4</Typography>
-        <Typography variant="h5">Heading 5</Typography>
+        <Typography variant="h5">Heading 5</Typography> */}
         <Typography variant="h6">Heading 6</Typography>
         <Typography>Lorem ipsum dolor sit amet.</Typography>
       </Box>
